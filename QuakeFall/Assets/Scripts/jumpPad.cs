457f3712677030328305cb72acae2118f78a5212
Szymon-Lewandowski <slewandowski@elam.co.uk>
2018-12-11 15:33:58 +0000

    JumpPad Script:
    Works fine. When the player collides with an object with the script, jumpForce Up gets added to the player. Resulting in a jump (force of the jumpPad can be changed).

---
+++ QuakeFall/Assets/Scripts/jumpPad.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jumpPad : MonoBehaviour {
+
+    public int jumpForce;
+ 
+    private void OnTriggerEnter(Collider other)
+    {
+        other.GetComponent<Rigidbody>().AddForce(Vector3.up * jumpForce);
+    }
+        
+}
