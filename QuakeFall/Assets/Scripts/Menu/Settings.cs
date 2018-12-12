a35026f5297ee12ce8fedb930ec1d05b0a6e8de2
Raiyana Haque <32648979+RaiyanaHaque@users.noreply.github.com>
2018-12-12 10:46:13 +0000

    RH - Updated Main Menu
    Experimented with implementing brightness slider and mouse sensitivity.

--- QuakeFall/Assets/Scripts/Menu/Settings.cs
+++ QuakeFall/Assets/Scripts/Menu/Settings.cs
@@ -6,6 +6,9 @@ namespace Menu
 	public class Settings : MonoBehaviour
 	{
 		[SerializeField] private Slider _fovSlider;
+		[SerializeField] private Slider _brightnessSlider;
+
+		private float _rbgValue = 0.5f;
 		
 		//Sound and music needed to experiment with linking the slider 
 		public void SetVolume(float volume)
@@ -14,6 +17,11 @@ namespace Menu
 		
 		}
 
+			public void AdjustAmbientLight (float rbgValue){
+				RenderSettings.ambientLight = new Color (rbgValue, rbgValue, rbgValue, 1);
+				PlayerPrefs.SetFloat("Realtime Brightness", _brightnessSlider.value);
+			}
+		
 		public void SetQuality(int qualityIndex)
 		{
 			//Connected to the quality settings of Unity
@@ -31,6 +39,7 @@ namespace Menu
 			PlayerPrefs.SetFloat("Realtime FOV", _fovSlider.value);
 		}
 
+		
 		public void Revert()
 		{
 			_fovSlider.value = PlayerPrefs.GetFloat("Applied FOV");
