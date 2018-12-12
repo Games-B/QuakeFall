51f1fb46752aca1d6330bb2449f6b2e60c5c17a5
Justinas Grigas <jgrigas@elam.co.uk>
2018-12-12 10:55:20 +0000

    adding health script

---
+++ QuakeFall/Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Player
+{
+	// Make sure the health script is attached to a player.
+	[RequireComponent(typeof(PlayerMovement))]
+	public class PlayerHealth : MonoBehaviour
+	{
+		[SerializeField, Range(1, 1000)] private int _maxHealth;
+	}
+}
