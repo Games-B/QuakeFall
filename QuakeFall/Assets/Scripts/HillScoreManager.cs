4a5931d9791273e4b083f068609ccdbf279759a7
shaafie <44161590+shaafie@users.noreply.github.com>
2018-12-13 09:45:48 +0000

    Basic King of The Hill Concept

---
+++ QuakeFall/Assets/Scripts/HillScoreManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HillScoreManager : MonoBehaviour
+{
+
+	public static int score;
+	Text text1;
+
+
+	void Awake()
+	{
+		text1 = GetComponent<Text>();
+		score = 0;
+	} 
+	
+	void Update()
+	{
+		text1.text = "Score" + score;
+	}
+}
---
+++ QuakeFall/Assets/Scripts/HillZone.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HillZone : MonoBehaviour {
+
+	void OnTriggerStay(Collider other)
+	{
+		HillScoreManager.score += 1;
+	}
+	
+}
