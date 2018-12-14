403a4ba0ff72941824e019c8e6fa8aa6917e5dd7
Raiyana Haque <haque.raiyana@outlook.com>
2018-12-14 11:47:30 +0000

    Messing stuff
    Messing with drawing shield

--- QuakeFall/Assets/Scripts/Player/TestPlayerHealth.cs
+++ QuakeFall/Assets/Scripts/Player/TestPlayerHealth.cs
@@ -6,31 +6,62 @@ using UnityEngine.UI;
 public class TestPlayerHealth : MonoBehaviour
 {
 
-	public int PlayerHealth; 
+	public int PlayerHealth;
 	public int NumOfHealth; //Number of health bars
 	public Image[] HealthBars;
+	public Image[] ShieldBars;
 	public Sprite RedBars; //Red health
-	public Sprite BlueBars;	//Blue shield bars
+	public Sprite BlueBars; //Blue shield bars
 
-	
-	void Update () {
+
+
+	private void Start()
+	{
+
+	}
+
+	void Update()
+	{
 
 		if (PlayerHealth > NumOfHealth)
 		{
 			PlayerHealth = NumOfHealth;
 		}
-		
-		for (int i = 0; i < HealthBars.Length; i++)
+
+		/*if (Input.GetKeyDown(KeyCode.Q))
 		{
-			if (i < NumOfHealth)
+			for (int i = 0; i < HealthBars.Length; i++)
 			{
-				HealthBars[i].enabled = true; 
-			}
-			else
+				if (i < NumOfHealth)
+				{
+					ShieldBars[i].enabled = true;
+				}
+				else
+				{
+					ShieldBars[i].enabled = false;
+				}
+			} */ //Disregard this
+
+			for (int i = 0; i < HealthBars.Length; i++)
 			{
-				HealthBars[i].enabled = false;
+				if (i < NumOfHealth)
+				{
+					HealthBars[i].enabled = true;
+				}
+				else
+				{
+					HealthBars[i].enabled = false;
+				}
 			}
+
+			// ***** NOTES TO SELF ***** //
+			// If the player picks up the shield
+			// Replace the red bars with blue
+			// When taken dmg, replace the blue ones with red
+			// Once shield gone, if taken dmg, get rid of the red ones
+			// When nothing is drawn, player = dead
+
 		}
-		
 	}
-}
+
+
