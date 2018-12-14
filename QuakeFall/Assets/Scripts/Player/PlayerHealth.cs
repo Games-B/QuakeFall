ac59c5eebfec83f845a2d04e3c7103fb9a0ca4d6
Justin <jgrigas@elam.co.uk>
2018-12-14 11:49:42 +0000

    Players can now be healed.

--- QuakeFall/Assets/Scripts/Player/PlayerHealth.cs
+++ QuakeFall/Assets/Scripts/Player/PlayerHealth.cs
@@ -7,5 +7,24 @@ namespace Player
 	public class PlayerHealth : MonoBehaviour
 	{
 		[SerializeField, Range(1, 1000)] private int _maxHealth;
+		[SerializeField] private int _currentHealth;
+
+		[SerializeField, Range(0, 1000)] private int _maxShield;
+		[SerializeField] private int _currentShield;
+
+		// Heals the player's health.
+		private void Heal(int amount)
+		{
+			// Work out the amount to heal, and clamp it to prevent over healing.
+			var amountToHeal = Mathf.Clamp(amount, 0, _currentHealth - _maxHealth);
+			_currentHealth += amountToHeal;
+		}
+
+		private void Shield(int amount)
+		{
+			// Work out the amount to heal, and clamp it to prevent over healing.
+			var amountToShield = Mathf.Clamp(amount, 0, _maxShield - _currentShield);
+			_currentShield  += amountToShield;
+		}
 	}
 }
