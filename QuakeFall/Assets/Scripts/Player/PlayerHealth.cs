dcb5bad6aa0787c9fdacbf008877734bd78528ee
Justin <jgrigas@elam.co.uk>
2018-12-14 15:53:48 +0000

    Player health is now finished.
    - Finished the player health script.
    - Added player health script to the player prefab.

--- QuakeFall/Assets/Scripts/Player/PlayerHealth.cs
+++ QuakeFall/Assets/Scripts/Player/PlayerHealth.cs
@@ -26,5 +26,40 @@ namespace Player
 			var amountToShield = Mathf.Clamp(amount, 0, _maxShield - _currentShield);
 			_currentShield  += amountToShield;
 		}
+
+		private void HurtHealth(int amount)
+		{
+			var amountToHurt = Mathf.Clamp(amount, 0, _currentHealth);
+			_currentHealth -= amountToHurt;
+			
+			// Check if the player should die.
+			if (_currentHealth <= 0)
+			{
+				Die();
+			}
+		}
+
+		private int HurtShield(int amount)
+		{
+			var amountToHurt = Mathf.Clamp(amount, 0, _currentShield);
+			_currentShield -= amountToHurt;
+			
+			// Return the amount of damage left.
+			return amount - amountToHurt;
+		}
+
+		public void Hurt(int amount)
+		{
+			// First try to hurt the shield.
+			var damageLeft = HurtShield(amount);
+			// Then hurt the health.
+			HurtHealth(damageLeft);
+		}
+
+		private void Die()
+		{
+			// Stuff that happens when you die.
+			print("U dEd PhAm!");
+		}
 	}
 }
