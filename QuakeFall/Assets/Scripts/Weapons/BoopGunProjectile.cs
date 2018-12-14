7c8a381f4b86ea7bb79803a444015bf4e90d62f9
Hassan Bahemia Areff <hbahemia@elam.co.uk>
2018-12-14 10:42:53 +0000

    fixed Boop Gun Knockback

--- QuakeFall/Assets/Scripts/Weapons/BoopGunProjectile.cs
+++ QuakeFall/Assets/Scripts/Weapons/BoopGunProjectile.cs
@@ -13,10 +13,22 @@ namespace Weapons
 		[SerializeField] private float _range;
 		[SerializeField] private float _knockBackRange;
 		[SerializeField] private Rigidbody[] _rigidBodies;
+		[SerializeField] private GameObject Ball;
+		
+		[SerializeField] private UnityEngine.Camera _fpsCam;
 
 		private void Start()
 		{
 			_rigidBodies = FindObjectsOfType<Rigidbody>();
+		}
+		
+		// Unity Methods.
+		private void Update () 
+		{
+			if (Input.GetKeyDown(KeyCode.Mouse0))
+			{
+				Shoot(Ball);
+			}
 		}
 
 		private void OnCollisionEnter(Collision other)
@@ -27,5 +39,16 @@ namespace Weapons
 				rigidBody.AddExplosionForce(_knockBack, transform.position, _knockBackRange);
 			}
 		}
+		
+		// Custom Methods.
+		private void Shoot(GameObject ball)
+		{
+			RaycastHit hit;
+			if (Physics.Raycast(_fpsCam.transform.position, _fpsCam.transform.forward, out hit, _range))
+			{
+				Debug.Log(hit.transform.name);
+			}
+		}
+		
 	}
 }
