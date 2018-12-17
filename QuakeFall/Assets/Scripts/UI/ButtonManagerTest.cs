8f9d436ed14d2fbb97289206420bc5c5c4e03f57
Raiyana Haque <haque.raiyana@outlook.com>
2018-12-17 15:39:57 +0000

    Mainmenu and options panel

--- QuakeFall/Assets/Scripts/UI/ButtonManagerTest.cs
+++ QuakeFall/Assets/Scripts/UI/ButtonManagerTest.cs
@@ -17,6 +17,8 @@ public class ButtonManagerTest : MonoBehaviour
 	void Start ()
 	{
 		OptionsPanel.SetActive(false);
+		GraphicsPanel.SetActive(false);
+		SoundPanel.SetActive(false);
 	}
 	
 	// Update is called once per frame
@@ -28,6 +30,30 @@ public class ButtonManagerTest : MonoBehaviour
 	{
 		OptionsPanel.SetActive(true);
 	}
-	
-	public 
+
+	public void Gameplay()
+	{
+		GameplayPanel.SetActive(true);
+		GraphicsPanel.SetActive(false);
+		SoundPanel.SetActive(false);
+	}
+
+	public void Graphics()
+	{
+		GraphicsPanel.SetActive(true);
+		GameplayPanel.SetActive(false);
+		SoundPanel.SetActive(false);
+	}
+
+	public void Sound()
+	{
+		SoundPanel.SetActive(true);
+		GraphicsPanel.SetActive(false);
+		GameplayPanel.SetActive(false);
+	}
+
+	public void BackSettingsPanel()
+	{
+		OptionsPanel.SetActive(false);
+	}
 }
