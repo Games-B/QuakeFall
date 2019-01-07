3b3436e1a27b7d06effd014de94ab105d26c714c
Justin <jgrigas@elam.co.uk>
2019-01-07 11:20:02 +0000

    Added local player support

---
+++ QuakeFall/Assets/Scripts/Network/LocalPlayer.cs
@@ -0,0 +1,23 @@
+﻿using Player;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace Network
+{
+	public class LocalPlayer : NetworkBehaviour
+	{
+		[SerializeField] private PlayerMovement _playerMovement;
+		[SerializeField] private UnityEngine.Camera _camera;
+		[SerializeField] private PlayerCamera _playerCamera;
+		
+		private void Start()
+		{
+			// Only work for local players.
+			if (!isLocalPlayer) return;
+			// 
+			_playerMovement.enabled = true;
+			_camera.enabled = true;
+			_playerCamera.enabled = true;
+		}
+	}
+}
