d24e833ff1cc64503f5f407f5a625ea47cde9fb3
Justin <jgrigas@elam.co.uk>
2019-01-07 13:43:12 +0000

    Working on weapons

--- QuakeFall/Assets/Scripts/Weapons/RayCastWeapon.cs
+++ QuakeFall/Assets/Scripts/Weapons/RayCastWeapon.cs
@@ -5,19 +5,23 @@ namespace Weapons
 {
 	public class RayCastWeapon : Weapon
 	{
+		[Header("RayCast Weapon"), SerializeField] private int _damage;
+		[SerializeField] private float _knockBack;
+		
 		protected override bool Shoot(UnityEngine.Camera targetCamera, out Vector3 targetPoint, out RaycastHit hit)
 		{
 			if (!base.Shoot(targetCamera, out targetPoint, out hit)) return false;
+			
 			if (hit.transform.CompareTag("Player"))
 			{
 				// Hurt the player.
-				hit.transform.GetComponent<PlayerHealth>().Hurt(Damage);
+				hit.transform.GetComponent<PlayerHealth>().Hurt(_damage);
 			}
 
-			// Push the object back.
+			// Push the object back if it has a rigid body.
 			if (hit.transform.GetComponent<Rigidbody>() != null)
 			{
-				hit.transform.GetComponent<Rigidbody>().AddForce(-hit.normal * KnockBack);
+				hit.transform.GetComponent<Rigidbody>().AddForce(-hit.normal * _knockBack);
 			}
 
 			return true;
--- QuakeFall/Assets/Scripts/Weapons/Weapon.cs
+++ QuakeFall/Assets/Scripts/Weapons/Weapon.cs
@@ -6,17 +6,15 @@ namespace Weapons
 	public class Weapon : MonoBehaviour
 	{
 		[Header("Weapon"), SerializeField] protected string Name;
+		[SerializeField] protected Vector2 Recoil;
 		[SerializeField] protected Sprite Icon;
 		[SerializeField] protected bool Enabled;
-		[SerializeField] protected GameObject Model;
-		[SerializeField] protected int Damage;
-		[SerializeField] protected float KnockBack;
 		[SerializeField] protected float Range;
 		[SerializeField] protected int Ammo;
 		[SerializeField] protected float FireRate;
 		[SerializeField] protected Transform GunEnd;
 
-		protected float TimeSinceShot;
+		private float _timeSinceShot;
 
 		// Getters and Setters.
 		public void SetEnabled(bool isEnabled)
@@ -26,14 +24,17 @@ namespace Weapons
 
 		protected virtual bool Shoot(UnityEngine.Camera targetCamera, out Vector3 targetPoint, out RaycastHit hit)
 		{
-			// Set up the out parameters.
-			targetPoint = Vector3.zero;
-			hit = new RaycastHit();
+			// Don't shoot if enough time has not passed or you don't have enough ammo or you don't have the weapon.
+			if (_timeSinceShot < FireRate || Ammo < 1 || !Enabled)
+			{
+				targetPoint = Vector3.zero;
+				hit = new RaycastHit();
+				return false;
+			}
+			_timeSinceShot = 0;
+			Ammo--;
 			
-			// Don't shoot if enough time has not passed.
-			if (!(TimeSinceShot >= FireRate)) return false;
-			TimeSinceShot = 0;
-			
+			// Set the start of the ray cast to the center of the camera, to make sure it points to the cross hair.
 			var rayOrigin = targetCamera.ViewportToWorldPoint(new Vector3(.5f, .5f, 0));
 			// Check if the ray cast hits anything.
 			if (Physics.Raycast(rayOrigin, targetCamera.transform.forward, out hit, Range))
