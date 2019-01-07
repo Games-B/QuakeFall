f22fe73311f3e5b1565573c77e872407dacb22ae
Hassan Bahemia Areff <hbahemia@elam.co.uk>
2019-01-07 12:16:48 +0000

    Added AimDownSight Script

---
+++ QuakeFall/Assets/Scripts/Weapons/AimDownSight.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class AimDownSight : MonoBehaviour
+{
+	[SerializeField] private Vector3 _aimDownSight;
+	[SerializeField] private Vector3 _hipFire;
+	[SerializeField, Range(0, 1)] private float _speed;
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKey(KeyCode.Mouse1))
+		{
+			transform.localPosition = Vector3.Slerp(transform.localPosition, _aimDownSight, _speed);
+		}
+		else
+		{
+			transform.localPosition = Vector3.Slerp(transform.localPosition, _hipFire, _speed);
+		}
+	}
+}
