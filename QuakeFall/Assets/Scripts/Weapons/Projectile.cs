e72eb5cdf8b1c61d2e0e035386de0f58bcf747b2
Justin <jgrigas@elam.co.uk>
2019-01-08 17:20:46 +0000

    Working on Boopgun

--- QuakeFall/Assets/Scripts/Weapons/Projectile.cs
+++ QuakeFall/Assets/Scripts/Weapons/Projectile.cs
@@ -6,13 +6,13 @@ namespace Weapons
 	public class Projectile : MonoBehaviour
 	{
 		[SerializeField] private int _damage;
-		[SerializeField] private float _range;
+		[SerializeField] private float _explosionRange;
 		[SerializeField] private float _explosionForce;
 
 		private void OnCollisionEnter()
 		{
 			// Get all the objects in the range of the explosion.
-			var colliderArray = Physics.OverlapSphere(transform.position, _range, 0);
+			var colliderArray = Physics.OverlapSphere(transform.position, _explosionRange, 0);
 			
 			// Go through all the objects.
 			foreach (var coll in colliderArray)
@@ -26,7 +26,7 @@ namespace Weapons
 				// Push the object back if it has a rigid body.
 				if (coll.GetComponent<Rigidbody>() != null)
 				{
-					coll.GetComponent<Rigidbody>().AddExplosionForce(_explosionForce, transform.position, _range);
+					coll.GetComponent<Rigidbody>().AddExplosionForce(_explosionForce, transform.position, _explosionRange);
 				}
 			}
 		}
