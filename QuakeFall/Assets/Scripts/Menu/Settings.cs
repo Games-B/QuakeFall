c48ab3b34480cf603a82be6383a91f9e1359e082
Raiyana Haque <haque.raiyana@outlook.com>
2019-01-09 10:47:51 +0000

    Main menu and settings

--- QuakeFall/Assets/Scripts/Menu/Settings.cs
+++ QuakeFall/Assets/Scripts/Menu/Settings.cs
@@ -3,19 +3,20 @@ using UnityEngine.UI;
 
 namespace Menu
 {
+
 	public class Settings : MonoBehaviour
 	{
 		[SerializeField] private Slider _fovSlider;
 
-		
+
 		//Sound and music needed to experiment with linking the slider 
 		public void SetVolume(float volume) //For in game master audio use AudioListener.Volume
 		{
-			Debug.Log(volume); 
-		
+			Debug.Log(volume);
+
 		}
 
-		
+
 		public void SetQuality(int qualityIndex)
 		{
 			//Connected to the quality settings of Unity
@@ -33,12 +34,12 @@ namespace Menu
 			PlayerPrefs.SetFloat("Realtime FOV", _fovSlider.value);
 		}
 
-		
+
 		public void Revert()
 		{
 			_fovSlider.value = PlayerPrefs.GetFloat("Applied FOV");
 		}
-		
+
 		public void Apply()
 		{
 			PlayerPrefs.SetFloat("Applied FOV", _fovSlider.value);
