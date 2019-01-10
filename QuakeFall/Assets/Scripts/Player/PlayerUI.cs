fdb8d8bd03d46b41ccfac9a7697d0c31932c1228
Hassan Bahemia Areff <hbahemia@elam.co.uk>
2019-01-10 10:32:24 +0000

    Scoreboard script added

---
+++ QuakeFall/Assets/Scripts/Player/PlayerUI.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+	public class PlayerUI: MonoBehaviour
+	{
+
+		[SerializeField] private GameObject scoreBoard;
+
+		// Use this for initialization
+		void Start ()
+		{
+
+		}
+	
+		// Update is called once per frame
+		void Update()
+		{
+
+			if (Input.GetKeyDown(KeyCode.Tab))
+			{
+				scoreBoard.SetActive(true);
+			}
+			else if (Input.GetKeyUp(KeyCode.Tab))
+			{
+				scoreBoard.SetActive(false);
+			}
+		}
+	}
+	
