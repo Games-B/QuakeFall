5d4e65be4281f5a8edd53fb413f94e765e36d1b6
Hassan Bahemia Areff <hbahemia@elam.co.uk>
2019-01-11 11:54:04 +0000

    Ignore

--- QuakeFall/Assets/Scripts/Network/HostGame.cs
+++ QuakeFall/Assets/Scripts/Network/HostGame.cs
@@ -6,7 +6,7 @@ public class HostGame : MonoBehaviour
 {
 	[SerializeField] private uint _roomSize = 16;
 
-	[SerializeField] private string _roomname;
+	[SerializeField] private string _roomName;
 
 	[SerializeField] private NetworkManager _networkManager;
 
@@ -21,15 +21,15 @@ public class HostGame : MonoBehaviour
 
 	public void SetRoomName(string _name)
 	{
-		_roomname = _name;
+		_roomName = _name;
 	}
 
 	public void CreateRoom()
 	{
-		if (_roomname != "" && _roomname != null)
+		if (_roomName != "" && _roomName != null)
 		{
-			Debug.Log("Creating Room: " + _roomname + " with room for " + _roomSize + " players.");
-			_networkManager.matchMaker.CreateMatch(_roomname, _roomSize, true, "", _networkManager.OnMatchCreate);
+			Debug.Log("Creating Room: " + _roomName + " with room for " + _roomSize + " players.");
+			_networkManager.matchMaker.CreateMatch(_roomName, _roomSize, true, "","","",0,0, _networkManager.OnMatchCreate);
 		}
 	}
 }
