7500d1d58ed722ea775d6d8bbddc69613d5a0bc9
Hassan Bahemia Areff <hbahemia@elam.co.uk>
2019-01-11 11:52:25 +0000

    adding host game functionality

--- QuakeFall/Assets/Scripts/Network/HostGame.cs
+++ QuakeFall/Assets/Scripts/Network/HostGame.cs
@@ -1,16 +1,35 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.Networking;
 
-public class HostGame : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+public class HostGame : MonoBehaviour
+{
+	[SerializeField] private uint _roomSize = 16;
+
+	[SerializeField] private string _roomname;
+
+	[SerializeField] private NetworkManager _networkManager;
+
+	private void Start()
+	{
+		_networkManager = NetworkManager.singleton;
+		if (_networkManager.matchMaker == null)
+		{
+			_networkManager.StartMatchMaker();
+		}
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	public void SetRoomName(string _name)
+	{
+		_roomname = _name;
+	}
+
+	public void CreateRoom()
+	{
+		if (_roomname != "" && _roomname != null)
+		{
+			Debug.Log("Creating Room: " + _roomname + " with room for " + _roomSize + " players.");
+			_networkManager.matchMaker.CreateMatch(_roomname, _roomSize, true, "", _networkManager.OnMatchCreate);
+		}
 	}
 }
