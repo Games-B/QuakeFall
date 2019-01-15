14cd8f6d5b54595d32b08a404d1394dd27d4a82a
Raiyana Haque <haque.raiyana@outlook.com>
2019-01-15 16:11:03 +0000

    Improved main menu with networking

---
+++ QuakeFall/Assets/Scripts/UI/HostGame.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+namespace UI
+{
+	public class HostGame : MonoBehaviour
+	{
+		[SerializeField] private NetworkManager _networkManager;
+		[SerializeField] private int _port = 7777;
+
+		public void UpdatePort(InputField target)
+		{
+			_port = int.Parse(target.text);
+		}
+
+		public void Host()
+		{
+			_networkManager.matchPort = _port;
+			_networkManager.StartHost();
+		}
+	}
+}
---
+++ QuakeFall/Assets/Scripts/UI/JoinGame.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+namespace UI
+{
+	public class JoinGame : MonoBehaviour
+	{
+		[SerializeField] private NetworkManager _networkManager;
+		[SerializeField] private string _ipAddress = "192.168.0.1";
+		[SerializeField] private int _port = 7777;
+		
+		public void UpdateIpAddress(InputField target)
+		{
+			_ipAddress = target.text;
+		}
+		
+		public void UpdatePort(InputField target)
+		{
+			_port = int.Parse(target.text);
+		}
+		
+		public void Join()
+		{
+			_networkManager.networkAddress = _ipAddress;
+			_networkManager.networkPort = _port;
+			_networkManager.StartHost();
+		}
+	}
+}
