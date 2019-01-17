89eb1bc28303aa7901e5c618f31b1d92fcc450b2
Justin <jgrigas@elam.co.uk>
2019-01-17 10:56:32 +0000

    Auto stash before merge of "master" and "origin/master"

---
+++ QuakeFall/Assets/Scripts/Network/NetworkCommands.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+namespace Network
+{
+	public class NetworkCommands : NetworkBehaviour
+	{
+		private NetworkManager _networkManager;
+
+		private void Awake()
+		{
+			_networkManager = FindObjectOfType<NetworkManager>();
+		}
+
+		[Command]
+		public void CmdShoot(int prefabIndex, Vector3 startPosition, Vector3 targetPoint, float initialForce)
+		{
+			RpcSpawnProjectile(prefabIndex, startPosition, targetPoint, initialForce);
+		}
+		
+		[ClientRpc]
+		public void RpcSpawnProjectile(int prefabIndex, Vector3 startPosition, Vector3 targetPoint, float initialForce)
+		{
+			var newProjectile = Instantiate(_networkManager.spawnPrefabs[prefabIndex], startPosition, Quaternion.identity, null);
+			
+			// Point it towards the target object.
+			newProjectile.transform.LookAt(targetPoint);
+			newProjectile.GetComponent<Rigidbody>().AddForce(newProjectile.transform.forward * initialForce);
+			NetworkServer.Spawn(newProjectile);
+		}
+	}
+}
--- QuakeFall/Assets/Scripts/Weapons/ProjectileWeapon.cs
+++ QuakeFall/Assets/Scripts/Weapons/ProjectileWeapon.cs
@@ -1,29 +1,19 @@
-﻿using UnityEngine;
+﻿using Network;
+using UnityEngine;
 using UnityEngine.Networking;
 
 namespace Weapons
 {
 	public class ProjectileWeapon : Weapon
 	{
-		[Header("Projectile Weapon"), SerializeField] private GameObject _projectilePrefab;
+		[Header("Projectile Weapon"), SerializeField] private int _prefabIndex;
 		[SerializeField] private float _initialForce;
-
-		[Command]
-		public void CmdSpawn(GameObject item)
-		{
-			NetworkServer.Spawn(item);
-		}
+		[SerializeField] private NetworkCommands _networkCommands;
 		
 		public override bool Shoot(UnityEngine.Camera targetCamera, out Vector3 targetPoint, out RaycastHit hit)
 		{
 			if (!base.Shoot(targetCamera, out targetPoint, out hit)) return false;
-			
-			var newProjectile = Instantiate(_projectilePrefab, GunEnd.position, Quaternion.identity, null);
-			
-			// Point it towards the target object.
-			newProjectile.transform.LookAt(targetPoint);
-			newProjectile.GetComponent<Rigidbody>().AddForce(newProjectile.transform.forward * _initialForce);
-			CmdSpawn(newProjectile);
+			_networkCommands.CmdShoot(_prefabIndex, GunEnd.position, targetPoint, _initialForce);
 			return true;
 		}
 	}
