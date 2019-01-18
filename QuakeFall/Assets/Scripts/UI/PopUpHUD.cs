fd627352736c7d038e7d400ba8501f506e3a227b
Raiyana Haque <haque.raiyana@outlook.com>
2019-01-18 11:50:54 +0000

    Script for Pop Up

---
+++ QuakeFall/Assets/Scripts/UI/PopUpHUD.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopUpHUD : MonoBehaviour
+{
+
+	public GameObject InGameHUD;
+	private bool _isShowing;
+
+	// Use this for initialization
+	void Start ()
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown(KeyCode.Escape))
+		{
+			_isShowing = !_isShowing;
+			InGameHUD.SetActive(_isShowing);
+		} 
+		
+	
+		
+	}
+}
