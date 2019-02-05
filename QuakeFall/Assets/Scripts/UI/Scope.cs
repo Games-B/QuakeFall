19f9fa95f8d1fb263aee23c9ecd6f8b40e0b32f3
Hassan Bahemia Areff <hbahemia@elam.co.uk>
2019-02-05 15:08:38 +0000

    Added Template for sniper

--- QuakeFall/Assets/Scripts/UI/Scope.cs
+++ QuakeFall/Assets/Scripts/UI/Scope.cs
@@ -2,18 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
 namespace UI
 {
 	public class Scope : MonoBehaviour
 	{
 
 		[SerializeField] private Animator _animator;
+		[SerializeField] private bool _isScoped = false;
 	
 		// Update is called once per frame
-		void Update () 
+		void Update ()
 		{
-		
+			if (Input.GetKeyDown(KeyCode.Mouse1))
+			{
+				_isScoped = !_isScoped;
+				_animator.SetBool("ScopedIn", _isScoped);
+			}
 		}
 	}
 }
