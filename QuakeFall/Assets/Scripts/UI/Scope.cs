a0ed190cb1c65dd9dd68c0271b8654af01e57162
Hassan Bahemia Areff <hbahemia@elam.co.uk>
2019-02-07 10:46:51 +0000

    Fixed Sniper added script

--- QuakeFall/Assets/Scripts/UI/Scope.cs
+++ QuakeFall/Assets/Scripts/UI/Scope.cs
@@ -6,18 +6,49 @@ namespace UI
 {
 	public class Scope : MonoBehaviour
 	{
-
+		[SerializeField] private Camera _targetCamera;
+		[SerializeField] private float _zoomedFov, _normalFov;
 		[SerializeField] private Animator _animator;
 		[SerializeField] private bool _isScoped = false;
+		[SerializeField] private float _scopeTime;
+		[SerializeField] private GameObject _scopeUi;
+		[SerializeField] private GameObject _weaponCamera;
+
+		private float _scopeTimer;
 	
 		// Update is called once per frame
-		void Update ()
+		private void Update ()
 		{
 			if (Input.GetKeyDown(KeyCode.Mouse1))
 			{
 				_isScoped = !_isScoped;
 				_animator.SetBool("ScopedIn", _isScoped);
+				
+				if (!_isScoped) OnUnscoped();
 			}
+
+			if (_isScoped)
+			{
+				if (_scopeTimer >= _scopeTime) OnScoped();
+				else
+				{
+					_scopeTimer = Mathf.Clamp(_scopeTimer + Time.deltaTime, 0, _scopeTime);
+				}
+			}
+		}
+
+		private void OnUnscoped()
+		{
+			_targetCamera.fieldOfView = _normalFov;
+			_scopeUi.SetActive(false);
+			_weaponCamera.SetActive(true);
+		}
+
+		private void OnScoped()
+		{
+			_targetCamera.fieldOfView = _zoomedFov;
+			_scopeUi.SetActive(true);
+			_weaponCamera.SetActive(false);
 		}
 	}
 }
